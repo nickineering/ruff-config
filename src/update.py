75ead6d1758aed9ceacaad54074cc39a8ffe1feb
Nicholas Ferrara <nferrara100@gmail.com>
2024-03-26 21:32:52 +0000

    fix: copy correctly with logging

--- src/update.py
+++ src/update.py
@@ -1,9 +1,17 @@
+import logging
 import shutil
 from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
 
 
 def update_ruff_base() -> None:
     """Copy the ruff.toml file to the new package so it can be extended from."""
-    src_file = Path(__file__) / "nickineering-ruff-base.toml"
-    dest_file = Path.cwd() / "ruff.toml"
+    filename = "nickineering-ruff-base.toml"
+    src_file = Path(__file__).parent / filename
+    dest_file = Path.cwd() / filename
     shutil.copy(src_file, dest_file)
+    logging.info(
+        "Default ruff config copied to %s. You now need to extend it in ruff.toml.",
+        dest_file,
+    )
